26becc1dc683ae247b7f3735b4eb79f72fe546e0
vsparrow <khan@vsparrow.com>
2019-02-19 00:49:08 -0500

    022019 completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+  def initialize word
+    @word = word
+    @word_sorted = word.split("").sort
+  end
+
+  def match words
+    matches = []
+    length = @word_sorted.length
+    words.each do |word|
+      if word.length == length
+        result = (word.split("").sort <=> @word_sorted)
+        matches.push(word) if result == 0
+        # binding.pry
+      end
+    end
+    matches
+  end
+
+end
